40ab6fcf54e9b9fda15e3e55ebbe4aeadce1009b
David-Ramalho <tavaresramalho@hotmail.com>
2022-07-23 13:26:02 -0300

    Final work on the page
    The page looks good and it was nice to practice CSS a lot on this one.

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const inputs= document.querySelectorAll('.input');
-const buttons= document.querySelectorAll('.login_icons.icons_button');
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
 
-const handleFocus = ({target})=>{    
-   const span = target.previousElementSibling;
-   span.classList.add('span_active');
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
 }
 
-const handleFocusOut = ({target})=>{    
-    if(target.value == '') {
+const handleFocusOut = ({ target }) => {
+  if (target.value === '') {
     const span = target.previousElementSibling;
-    span.classList.remove('span_active');
-    }
- }
+    span.classList.remove('span-active');
+  }
+}
 
  //scripts
 
-inputs.forEach((input) =>{
-    input.addEventListener('focus',handleFocus)
-});
-
-inputs.forEach((input) =>{
-    input.addEventListener('focusout',handleFocusOut)
-});
-
+ const handleChange = () => {
+    const [username, password] = inputs;
+  
+    if (username.value && password.value.length >= 8) {
+      button.removeAttribute('disabled');
+    } else {
+      button.setAttribute('disabled', '');
+    }
+  }
+  
+  inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+  inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+  inputs.forEach((input) => input.addEventListener('input', handleChange));
 
